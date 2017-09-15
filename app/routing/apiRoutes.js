7e78f471f3764fcc53db9924e3e3e9a9b08aeb1e
alex-117 <alexulfich@gmail.com>
2017-09-14 21:32:46 -0600

    file update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,22 @@ module.exports = function(app) {
         res.json(reservations);
     });
     
-    app.post("/api/reservation", function(req, res) {
+    app.delete("/api/deletereservation/:id", function(req, res) {
+       console.log(req.params.id); 
+        reservations.forEach(function(content, index) {
+            if(content.reservationID === req.params.id) {
+                reservations.splice(index, 1);
+            }
+        })
         
+        res.send(reservations);
+    });
+    
+    app.post("/api/reservation", function(req, res) {
+        var newReservation = req.body;
+        
+        reservations.push(newReservation);
+        
+        res.json(newReservation);
     });
 }
